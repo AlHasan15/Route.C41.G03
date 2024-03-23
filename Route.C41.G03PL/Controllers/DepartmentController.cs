63fbca905962757f276d05a72304b91d8a40281c
AlHasan15 <alhassanrasmy1@gmail.com>
2024-03-23 23:22:21 +0200

    Add project files.

---
+++ Route.C41.G03PL/Controllers/DepartmentController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Route.C41.G03.BLL.Interfaces;
+using Route.C41.G03.Dal.Models;
+
+
+namespace Route.C41.G03PL.Controllers
+{
+    //ingertiance : DepartnentController is a Controller 
+    //Association : DepartnentController has a DepartmentRepository 
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+
+
+        // /Department/Index
+        public IActionResult Index()
+        {
+            var departments = _departmentRepository.GetAll();
+            return View(departments);
+        }
+
+        // /Department/Create
+        //[HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid) // Server side Validation
+            {
+               var count = _departmentRepository.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(department);
+        }
+    }
+}
