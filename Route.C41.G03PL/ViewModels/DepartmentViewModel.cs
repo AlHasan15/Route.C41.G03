9af2635785be5fa133d22a1ecfc7b6a9edf9d35c
AlHasan15 <alhassanrasmy1@gmail.com>
2024-04-03 08:14:08 +0200

    implment Department PartialView

---
+++ Route.C41.G03PL/ViewModels/DepartmentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Route.C41.G03.Dal.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Route.C41.G03PL.ViewModels
+{
+    public class DepartmentViewModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Date Of Creation")]
+        public DateTime DateOfCreation { get; set; }
+
+        // Navigational property => (Many) 
+        public ICollection<Employee> Employees { get; set; } = new HashSet<Employee>();
+    }
+}
